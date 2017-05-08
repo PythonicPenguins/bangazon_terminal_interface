c75888f1543f3154a1b2dd442f3113bd7e294e42
Meg Ducharme <ducharme.meg@gmail.com>
2017-05-08 08:32:14 -0500

    added customer manager unit tests

---
+++ test/TestCustomerManager.py
@@ -0,0 +1,37 @@
+import unittest
+import sys;sys.path.append('../src')
+from CustomerManager import *
+
+class TestCustomerManager(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.customer = {"first_name": "Meg", "last_name": "Ducharme", "address": "500 Interstate Blvd S", "phone_number": "4104561238"}
+
+
+
+    def test_customer_is_saved_to_database(self):
+
+        save_customer_to_database(self.customer.first_name, self.customer.last_name, self.customer.address, self.customer.phone_number)
+
+        customer_id = get_customer_id(self.customer.first_name, self.customer.last_name, self.customer.address, self.customer.phone_number)
+
+        self.assertIsNotNone(customer_id)
+
+
+    def test_get_all_customers(self):
+
+        save_customer_to_database(self.customer.first_name, self.customer.last_name, self.customer.address, self.customer.phone_number)
+
+        all_customers = get_all_customers()
+
+        self.assertTrue(len(all_customers) > 0)
+
+
+    def test_choose_active_customer(self):
+        activate_customer("3")
+
+        self.assertIsNotNull(active_customer)
+
+
+    def
